8871636c4571a74af999c6c49dcb8abb3a759373
Razhelq <mateuszszpakowski@wp.pl>
2018-10-24 21:12:54 +0200

    mad libs finished

--- Chapter 8 - Files/mad_libs/mad_libs.py
+++ Chapter 8 - Files/mad_libs/mad_libs.py
@@ -1,6 +1,22 @@
-adjective = input('Enter an adjective: ')
-noun = input('Enter a noun: ')
-verb = input('Enter a verb: ')
-noun = input('Enter a noun: ')
+text_file = open('text_to_fill.txt')
 
-text_file =
+words = text_file.read()
+words = words.split(' ')
+for word in words:
+    if word == 'ADJECTIVE':
+        words[words.index(word)] = input('Enter an adjective: ')
+    elif word == 'NOUN':
+        words[words.index(word)] = input('Enter a noun: ')
+    elif word == 'VERB':
+        words[words.index(word)] = input('Enter a verb: ')
+    elif word == 'ADVERB':
+        words[words.index(word)] = input('Enter a adverb: ')
+
+new_text = open('new_text.txt', 'w')
+new_text.write(' '.join(words))
+
+new_text.close()
+text_file.close()
+
+new_text = open('new_text.txt')
+print(new_text.read())
