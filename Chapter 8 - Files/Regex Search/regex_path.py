8e8b19bcce373d0cecfa8532da6a04968b576c03
Razhelq <mateuszszpakowski@wp.pl>
2018-10-30 06:48:10 +0100

    regex path finished

--- Chapter 8 - Files/Regex Search/regex_path.py
+++ Chapter 8 - Files/Regex Search/regex_path.py
@@ -1,6 +1,12 @@
-import os
+import os, re
 
-files = os.makedirs('.')
+files = os.listdir('./to_search')
 
 for file in files:
     print(file)
+    file_to_check = open('./to_search/{}'.format(file))
+    file_splited = file_to_check.readlines()
+    for line in file_splited:
+        if re.search(r'\W*(for)\W*', line) != None:
+            print(line)
+    file_to_check.close()
