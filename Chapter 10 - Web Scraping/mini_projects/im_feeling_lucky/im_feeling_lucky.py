f10358b7f1589089a282299069c26c7735641f9f
Razhelq <mateuszszpakowski@wp.pl>
2018-11-20 20:39:43 +0100

    lucky app finished

---
+++ Chapter 10 - Web Scraping/mini_projects/im_feeling_lucky/im_feeling_lucky.py
@@ -0,0 +1,23 @@
+# im_feeling_lucky.py - Opens several Google search results
+
+
+import request, sys, webbrowser, bs4
+
+
+print('Googling...')
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+
+
+# Open a browser tab for each result.
+link_elems = soup.select('.r a')
+
+
+# Open a browser tab for each result.
+num_open = min(5, len(link_elems))
+for i in range(num_open):
+    webbrowser.open('http://google.com' + link_elems[i].get('href'))
--- Chapter 10 - Web Scraping/mini_projects/mapit/mapit.py
+++ Chapter 10 - Web Scraping/mini_projects/mapit/mapit.py
@@ -1,5 +1,5 @@
 # ! python3
-# mapIt.py - Launches a map in the browser using an address from the command line or clipboard.
+# mapit.py - Launches a map in the browser using an address from the command line or clipboard.
 
 
 import webbrowser, sys, pyperclip
