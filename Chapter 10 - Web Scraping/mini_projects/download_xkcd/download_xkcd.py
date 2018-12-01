8fa5e7d937b0c2d7791e3e8e2a958b22a681b933
Razhelq <mateuszszpakowski@wp.pl>
2018-12-01 14:34:10 +0100

    xkcd app fixed

--- Chapter 10 - Web Scraping/mini_projects/download_xkcd/download_xkcd.py
+++ Chapter 10 - Web Scraping/mini_projects/download_xkcd/download_xkcd.py
@@ -1,6 +1,12 @@
 # download_xkcd.py - Downloads every single XKCD comic.
 
-import requests, os, bs4
+import requests, os, bs4, shutil
+from requests.exceptions import MissingSchema
+
+if os.path.isdir('xkcd') == True:
+    shutil.rmtree('xkcd')
+else:
+    os.makedirs('xkcd')
 
 url = 'http://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
@@ -11,6 +17,7 @@ while not url.endswith('#'):
 
     soup = bs4.BeautifulSoup(res.text)
     comic_elem = soup.select('#comic img')
+    print(comic_elem[0].get('src'))
     if comic_elem == []:
         print('Could not find comic image.')
     else:
@@ -19,17 +26,20 @@ while not url.endswith('#'):
             print('Downloading image {}...'.format(comic_url))
             res = requests.get(comic_url)
             res.raise_for_status()
-        except requests.exceptions.MissingSchema:
+        except MissingSchema:
             # skip this comic
             prev_link = soup.select('a[rel="prev"]')[0]
-            url = 'http://xkcd.com' + prev_link('href')
+
+            print(prev_link)
+            print(prev_link.get('href'))
+            url = 'http://xkcd.com' + prev_link.get('href')
             continue
-            image_file = open(os.path.join('xkcd', os.path.basement(comic_url)), 'wb')
-            for chunk in res.iter_content(100000):
-                image_file.write(chunk)
-            image_file.close()
-        prev_link = soup.select('a[rel="prev"]')[0]
-        url = 'http://xkcd.com' + prev_link.get('href')
+        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+    prev_link = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prev_link.get('href')
 
 print('Done')
 
