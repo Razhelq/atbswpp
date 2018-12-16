908afb8493f1d071738a2573611163ec80a2664a
Razhelq <mateuszszpakowski@wp.pl>
2018-12-16 14:41:25 +0100

    combine pdfs app done

--- Chapter 11 - PDF, XLSX, DOCS/mini projects/combine_pdfs.py
+++ Chapter 11 - PDF, XLSX, DOCS/mini projects/combine_pdfs.py
@@ -6,17 +6,24 @@ import PyPDF2, os
 
 # Get all the PDF filenames.
 pdf_files = []
-for filename in os.listdir('.'):
+for filename in os.listdir('..\\..\\automate_online-materials'):
     if filename.endswith('.pdf'):
-        pdf_files.append(filename
+        if not PyPDF2.PdfFileReader(open(os.path.join('..\\..\\automate_online-materials\\' + filename), 'rb')).isEncrypted:
+            pdf_files.append(filename)
 
-pdf_files.sort(key=str.lower())
+pdf_files.sort(key=str.lower)
 
 pdf_writer = PyPDF2.PdfFileWriter()
 
 # Loop trough all the PDF files.
 for filename in pdf_files:
-    pdf_file_obj = open(filename, 'rb')
+    pdf_file_obj = open(os.path.join('..\\..\\automate_online-materials\\' + filename), 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+    for page_num in range(1, pdf_reader.numPages):
+        page_obj = pdf_reader.getPage(page_num)
+        pdf_writer.addPage(page_obj)
 
+pdf_output = open('allminutes.pdf', 'wb')
+pdf_writer.write(pdf_output)
+pdf_output.close()
 
