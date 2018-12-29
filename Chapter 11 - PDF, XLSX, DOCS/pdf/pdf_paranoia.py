5fce66b52934b5bb5476a6158d1690349364b235
Razhelq <mateuszszpakowski@wp.pl>
2018-12-29 20:23:48 +0100

    removing initial files implemented

--- Chapter 11 - PDF, XLSX, DOCS/pdf/pdf_paranoia.py
+++ Chapter 11 - PDF, XLSX, DOCS/pdf/pdf_paranoia.py
@@ -13,7 +13,18 @@ for foldername, subfolders, filenames in os.walk('.'):
             for page_num in range(1, pdf_reader.numPages):
                 page_obj = pdf_reader.getPage(page_num)
                 pdf_writer.addPage(page_obj)
-            pdf_output = open('{}_encrypted.pdf'.format(filename), 'wb')
+            print(filename[:len(filename)-4])
+            pdf_output = open('{}_encrypted.pdf'.format(filename[:len(filename)-4]), 'wb')
             pdf_writer.encrypt(pw)
             pdf_writer.write(pdf_output)
             pdf_output.close()
+            pdf_file_obj.close()
+
+for foldername, subfolders, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename.endswith('_encrypted.pdf'):
+            if PyPDF2.PdfFileReader(open(os.path.join(os.getcwd() + '\\' + filename), 'rb')).isEncrypted:
+                pdf_reader = PyPDF2.PdfFileReader(open(os.path.join(os.getcwd() + '\\' + filename), 'rb'))
+                if pdf_reader.decrypt(pw):
+                    os.remove(filename[:len(filename)-14] + '.pdf')
+
