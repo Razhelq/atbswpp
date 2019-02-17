c2afbc4e2a29d7aa7f6b2857641a440ba8dee1c0
Razhelq <mateuszszpakowski@wp.pl>
2019-02-17 14:01:32 +0100

    aute unsubscriber corrected

--- Chapter 12 - Emails/auto_unsubscriber.py
+++ Chapter 12 - Emails/auto_unsubscriber.py
@@ -10,23 +10,25 @@ def check_for_email():
     imap_obj = imapclient.IMAPClient('imap.wp.pl', ssl=True)
     imap_obj.login('', '')
     imap_obj.select_folder('INBOX', readonly=True)
-    UIDs = imap_obj.search([u'SINCE', date(2015, 1, 1)])
+    UIDs = imap_obj.search([u'SINCE', date(2017, 1, 1)])
     domains = []
     for UID in UIDs:
         raw_mail = imap_obj.fetch([UID], ['BODY[]', 'FLAGS'])
         message = pyzmail.PyzMessage.factory(raw_mail[UID][b'BODY[]'])
         try:
             mess = message.html_part.get_payload().decode(message.html_part.charset)
-            if re.search(r'unsubscribe', str(mess)):
+            print(mess.get_subject())
+            if re.search(r'unsubscribe', mess):
                 try:
-                    domain = re.search(r'(?<=unsubscribe).*(?<=<a href=\"https://)(.+?(?=/))', mess).group(1)
+                    domain = re.search(r'(?<=unsubscribe).*((?<=<a href=\"https://)|(?<=<a href=\"http://))(.+?(?=/))', mess).group(2)
                     if domain not in domains:
                         domains.append(domain)
                         link = re.search(r'(?<=unsubscribe).*(?<=<a href=\")(.+?(?=\"))', mess).group(1)
+                        print(link)
                         webbrowser.open(link)
                 except:
                     pass
-        except AttributeError:
+        except:
             pass
 
 check_for_email()
