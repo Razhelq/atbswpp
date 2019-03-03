d6e805f0bbdea56f4aaac396dd375d5008568171
Razhelq <mateuszszpakowski@wp.pl>
2019-03-03 15:30:53 +0100

    stopwatch prettified with rjust and pyperclip

--- Chapter 11.5 - Time, Schedule/mini projects/super_stopwatch.py
+++ Chapter 11.5 - Time, Schedule/mini projects/super_stopwatch.py
@@ -1,10 +1,12 @@
 # stopwatch.py - A simple stopwatch program.
 
 
-import time
+import time, pyperclip
 
 
 # Display the program's instructions.
+import pyperclip as pyperclip
+
 print('Press enter to begin. Afterwards, press Enter to "click" the stopwatch. Press Ctrl-C to quit.')
 input()                     # Press enter to begin
 print('Started.')
@@ -18,7 +20,9 @@ try:
         input()
         lap_time = round(time.time() - last_time, 2)
         total_time = round(time.time() - start_time, 2)
-        print('Lap #{}: {}s ({}s)'.format(lap_num, total_time, lap_time), end='')
+        ret_string = ('Lap #{}: {}s ({}s)'.format(str(lap_num).rjust(2), str(total_time).rjust(5), str(lap_time).rjust(5)))
+        print(ret_string, end='')
+        pyperclip.copy(ret_string)
         lap_num += 1
         last_time = time.time() # resets the last lap time
 except:
